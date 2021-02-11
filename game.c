4503fe0e3f10e0be0df8204cb67d4b8a8df6f1ed
jpe230 <pablin.123.ra@gmail.com>
2021-02-11 01:22:14 -0600

    Controller

--- game.c
+++ game.c
@@ -11,7 +11,7 @@ int main(void)
 
     time_t now = time(NULL);
     struct tm tm_now;
-    
+
     char buff[100];
 
     InitWindow(screenWidth, screenHeight, "Clock");
@@ -92,9 +92,67 @@ int main(void)
 
         DrawTextureRec(lonk, frameRec, position, WHITE);
 
-        strftime(buff, sizeof(buff), "Hey Link, the time is %H %M", &tm_now) ;
+        strftime(buff, sizeof(buff), "Hey Link, the time is %H %M", &tm_now);
 
         DrawTextEx(fontTtf, buff, (Vector2){90.58f, 230.4f}, fontTtf.baseSize, 2, WHITE);
+
+        if (IsGamepadAvailable(GAMEPAD_PLAYER1))
+        {
+            DrawText(TextFormat("GP1: %s", GetGamepadName(GAMEPAD_PLAYER1)), 10, 10, 10, BLACK);
+            // Draw buttons: xbox home
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_MIDDLE))
+                DrawCircle(394, 89, 19, RED);
+
+            // Draw buttons: basic
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_MIDDLE_RIGHT))
+                DrawCircle(200, 150, 9, RED);
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_MIDDLE_LEFT))
+                DrawCircle(202, 150, 9, RED);
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_RIGHT_FACE_LEFT))
+                DrawCircle(201, 151, 15, BLUE);
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_RIGHT_FACE_DOWN))
+                DrawCircle(206, 187, 15, LIME);
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_RIGHT_FACE_RIGHT))
+                DrawCircle(102, 151, 15, MAROON);
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_RIGHT_FACE_UP))
+                DrawCircle(036, 115, 15, GOLD);
+
+            // Draw buttons: d-pad
+            DrawRectangle(317, 202, 19, 71, BLACK);
+            DrawRectangle(293, 228, 69, 19, BLACK);
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_LEFT_FACE_UP))
+                DrawRectangle(317, 202, 19, 26, RED);
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_LEFT_FACE_DOWN))
+                DrawRectangle(317, 202 + 45, 19, 26, RED);
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_LEFT_FACE_LEFT))
+                DrawRectangle(292, 228, 25, 19, RED);
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_LEFT_FACE_RIGHT))
+                DrawRectangle(292 + 44, 228, 26, 19, RED);
+
+            // Draw buttons: left-right back
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_LEFT_TRIGGER_1))
+                DrawCircle(259, 61, 20, RED);
+            if (IsGamepadButtonDown(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_RIGHT_TRIGGER_1))
+                DrawCircle(536, 61, 20, RED);
+
+            // Draw axis: left joystick
+            DrawCircle(259, 152, 39, BLACK);
+            DrawCircle(259, 152, 34, LIGHTGRAY);
+            DrawCircle(259 + (GetGamepadAxisMovement(GAMEPAD_PLAYER1, GAMEPAD_AXIS_LEFT_X) * 20),
+                       152 + (GetGamepadAxisMovement(GAMEPAD_PLAYER1, GAMEPAD_AXIS_LEFT_Y) * 20), 25, BLACK);
+
+            // Draw axis: right joystick
+            DrawCircle(461, 237, 38, BLACK);
+            DrawCircle(461, 237, 33, LIGHTGRAY);
+            DrawCircle(461 + (GetGamepadAxisMovement(GAMEPAD_PLAYER1, GAMEPAD_AXIS_RIGHT_X) * 20),
+                       237 + (GetGamepadAxisMovement(GAMEPAD_PLAYER1, GAMEPAD_AXIS_RIGHT_Y) * 20), 25, BLACK);
+
+            // Draw axis: left-right triggers
+            DrawRectangle(170, 30, 15, 70, GRAY);
+            DrawRectangle(604, 30, 15, 70, GRAY);
+            DrawRectangle(170, 30, 15, (((1.0f + GetGamepadAxisMovement(GAMEPAD_PLAYER1, GAMEPAD_AXIS_LEFT_TRIGGER)) / 2.0f) * 70), RED);
+            DrawRectangle(604, 30, 15, (((1.0f + GetGamepadAxisMovement(GAMEPAD_PLAYER1, GAMEPAD_AXIS_RIGHT_TRIGGER)) / 2.0f) * 70), RED);
+        }
 
         EndDrawing();
         //----------------------------------------------------------------------------------
